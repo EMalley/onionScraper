a02b9578ce02f6b520ebd863eaa2b1f11b65a04e
EMalley <EMalley998@gmail.com>
2019-03-26 20:42:57 -0400

    Handlebars responding

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var cheerio = require("cheerio");
 var db = require("./models");
 var PORT = 8080;
 var app = express();
+
+
+
 // Configure middleware
 
 // Use morgan logger for logging requests
@@ -21,72 +24,88 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/onionScraper", { useNewUrlParser: true });
 
-var result = [];
+// handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
 // ROUTES
 // ++++++++++++++++++++++++++++++++++++++++++++++
-// A GET route for scraping theOnion website
-axios.get("https://www.theonion.com/").then(function(response){
-    var $ = cheerio.load(response.data);
-    // console.log(JSON.stringify(response.data, null , 2));
-    // Grab a selected item from the website to scrape.
-    $(".js_entry-link").each(function(i, element){
-        // var result = {};
-        // Get text from every link and save them as properties of the result object
-        result.push({
-            title: $(this).children("a").val()
-            // link: $(this).children("a").attr("href")
-        })
+app.get("/", function(req, res) {
+    db.Article.find({})
+      .then(dbUser => {
+        res.render("index");
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
 
-        // result.title = $(this).text();
-        // result.link =$(this).children("a").attr('href');
-        console.log(result)
-        db.Article.create(result).then(function(dbArticle){
-            // console.log(dbArticle);
-        }).catch(function(err){
-            // console.log(err)
-        })
+
+app.get("/scraoe", function (req, res) {
+    axios.get("https://www.theonion.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        // Grab a selected item from the website to scrape.
+        $(".js_entry-link").each(function (i, element) {
+            var result = {};
+            // Get text from every link and save them as properties of the result object
+
+            result.title = $(this).text();
+            result.link = $(this).children("a").attr('href');
+            console.log(result)
+            db.Article.create(result).then(function (dbArticle) {
+                console.log(dbArticle);
+            }).catch(function (err) {
+                console.log(err)
+            })
+        });
+        res.send("Scrape Complete");
     });
-    // res.send("Scrape Complete");
 });
 
 // Route for getting articles from DB
-app.get("/articles", function(req, res){
+app.get("/articles", function (req, res) {
     var id = req.params.id
     db.Article.findById(id)
-    .then(function(dbArticle){
-        res.json(dbArticle)
-    }).catch(function(err){
-        res.json(err);
-    })
+        .then(function (dbArticle) {
+            res.json(dbArticle)
+        }).catch(function (err) {
+            res.json(err);
+        })
 });
 
 
 //Route to get an Article by its id and populate it with its note. 
-app.get("/articles/:id", function(req, res){
+app.get("/articles/:id", function (req, res) {
     var id = req.params.id;
     db.Article.findById(id)
-    .then(function(dbArticle){
-        res.json(dbArticle)
-    }).catch(function(err){
-        res.json(err)
-    });
+        .then(function (dbArticle) {
+            res.json(dbArticle)
+        }).catch(function (err) {
+            res.json(err)
+        });
 });
 
 
 //Route to Save a specific note from an article
-app.get("/articles/:id", function(req, res){
+app.get("/articles/:id", function (req, res) {
     db.Note.create(req.body)
-    .then(function(dbNote){
-        return db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote}).then(function (dbArticle){
-            res.json(dbArticle)
-        }).catch(function(err){
-            res.json(err)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote })
+                .then(function (dbArticle) {
+                    res.json(dbArticle)
+                }).catch(function (err) {
+                    res.json(err)
+                });
         });
-    });
 });
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
 
 // Start the server
 app.listen(PORT, function () {
     console.log("App running on port " + PORT);
-  });
-  
+});
+
