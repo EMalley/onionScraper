aa4acee6d6432c3520fa53427de6b1ccd541fe05
EMalley <EMalley998@gmail.com>
2019-04-08 18:04:44 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -35,6 +35,16 @@ else{
 }
 // --------------------------------------------------------------
 
+var db = mongoose.connection;
+
+db.on('error', function(err){
+    console.log("Mongoose Error: ", err);
+});
+
+db.once('open', function(){
+    console.log('Mongoose connection successful.');
+});
+
 // Routes 
 // ++++++++++++++++++++++++++++++++++++++++++++++
 
